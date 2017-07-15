35eaae4c872f57d51d50737e26233ab5547a9c3b
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2017-07-15 11:42:43 -0700

    Set up Basic Components and Integrated PropTypes

---
+++ src/Book.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default Book
---
+++ src/BookShelf.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+
+  static propTypes = {
+    bookshelf: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default BookShelf
